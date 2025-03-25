8d931f7f61349d03d6500e7ca50aa3f45ac34d18
Abuzar Ghafari <abuzarr.ghafari@gmail.com>
2025-03-26 03:20:38 +0500

    proxy  added

--- server.js
+++ server.js
@@ -15,6 +15,22 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+
+app.get('/api/proxy', async (req, res) => {
+    try {
+        const response = await axios.get('https://zplay1.in/sports/api/v1/events/matches/inplay', {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching API:', error.response ? error.response.data : error.message);
+        res.status(500).json({ error: 'Failed to fetch API' });
+    }
+});
+
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
